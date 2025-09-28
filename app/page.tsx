7d7581b8eb264dde5f6cf26d474cb1c1f2f582e7
Mostafiz <mostafiz4372@gmail.com>
2025-09-28 20:57:49 +0600

    added the toast sonner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export default function Home() {
+
+  const handleSonner = ()=> {
+    toast.success("Payment Successfull")
+  }
   return (
     <div className=" min-h-dvh flex flex-col justify-center gap-5 items-center text-5xl">
       <h2>WellCome to DevFolio</h2>
-      <Button>Click me</Button>
+      <Button onClick={()=>handleSonner()}>Click me</Button>
     </div>
   );
 }
