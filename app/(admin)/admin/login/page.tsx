e99ba2c0c392cec926ed113ec9cec7c3f9d7cf65
Mostafiz <mostafiz4372@gmail.com>
2025-09-29 22:52:04 +0600

    blog project dashboard and login page create in nextjs

---
+++ app/(admin)/admin/login/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AdminLoginPage() {
+  return (
+    <div>
+      <h2> This is admin Login page now</h2>
+    </div>
+  )
+}
---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,8 @@
+
+export default function DashboardLayout() {
+  return (
+    <div>
+        <h1>This is Dashboard page</h1>
+    </div>
+  )
+}
