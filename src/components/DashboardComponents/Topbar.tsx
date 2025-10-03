c0db272e79c90c42d17a67d04b641e0ef5503fc9
Mostafiz <mostafiz4372@gmail.com>
2025-10-03 23:26:29 +0600

    created signout user button

--- src/components/DashboardComponents/Topbar.tsx
+++ src/components/DashboardComponents/Topbar.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { User2, Bell } from "lucide-react";
+import { signOut } from "next-auth/react";
 
 export default function Topbar({ title = "Dashboard" }: { title?: string }) {
     const [open, setOpen] = useState(false);
@@ -35,7 +36,7 @@ export default function Topbar({ title = "Dashboard" }: { title?: string }) {
                             <button className="w-full text-left px-2 py-2 text-sm text-slate-200 hover:bg-zinc-800 rounded">
                                 Settings
                             </button>
-                            <button className="w-full text-left px-2 py-2 text-sm text-rose-400 hover:bg-zinc-800 rounded">
+                            <button onClick={()=>signOut()} className="w-full text-left px-2 py-2 text-sm text-rose-400 hover:bg-zinc-800 rounded">
                                 Sign out
                             </button>
                         </div>
